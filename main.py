7cbedd8b544e94d2d4419ef2e6a4c47b5ff5de0b
Dolgor03 <dolgoryandanova03@gmail.com>
2024-08-17 17:26:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print(12 + 89 - 1)
+# "1st program"
+print(5 * 9 ** 0.5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1001)
+# "3rd program"
+print(2 * 2 + 2 == 2 * (2 + 2))
+# "4th program"
+print(float('123.456'))
+print(float('123.456') * 10)
+print(float('123.456') * 100)
+print(float('123.456') * 1000)
+print(int(float('123.456') * 1000))
